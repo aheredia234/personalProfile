52fbb099a5cf5082fdc1b20273f56bc42134df7d
Angela Heredia <aheredia234@students.mchenry.edu>
2019-04-26 14:51:27 -0500

    Initial commit

---
+++ app/src/main/java/com/example/angie/a2interfaces/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.angie.a2interfaces;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button button = (Button)findViewById(R.id.btnViewProfile);
+        button.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                startActivity(new Intent(MainActivity.this, Profile.class));
+            }
+        });
+    }
+}
